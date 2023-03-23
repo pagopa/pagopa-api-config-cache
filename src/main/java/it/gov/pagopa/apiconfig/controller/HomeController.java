a99c93766c43908e100bd6d298885eb0f240ee76
Cesare Caccuri <cesare.caccuri@gmail.com>
2023-03-23 13:37:05 +0100

    more coverage

--- src/main/java/it/gov/pagopa/apiconfig/controller/HomeController.java
+++ src/main/java/it/gov/pagopa/apiconfig/controller/HomeController.java
@@ -1,7 +1,18 @@
 package it.gov.pagopa.apiconfig.controller;
 
 import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import it.gov.pagopa.apiconfig.model.AppInfo;
+import it.gov.pagopa.apiconfig.model.ProblemJson;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +25,15 @@ public class HomeController {
   @Value("${server.servlet.context-path}")
   String basePath;
 
+  @Value("${info.application.name}")
+  private String name;
+
+  @Value("${info.application.version}")
+  private String version;
+
+  @Value("${info.properties.environment}")
+  private String environment;
+
   /**
    * @return redirect to Swagger page documentation
    */
@@ -24,5 +44,65 @@ public class HomeController {
       basePath += "/";
     }
     return new RedirectView(basePath + "swagger-ui.html");
+  }
+
+  /**
+   * Health Check
+   *
+   * @return ok
+   */
+  @Operation(
+      summary = "Return OK if application is started",
+      security = {
+          @SecurityRequirement(name = "ApiKey"),
+          @SecurityRequirement(name = "Authorization")
+      },
+      tags = {"Home"})
+  @ApiResponses(
+      value = {
+          @ApiResponse(
+              responseCode = "200",
+              description = "OK",
+              content =
+              @Content(
+                  mediaType = MediaType.APPLICATION_JSON_VALUE,
+                  schema = @Schema(implementation = AppInfo.class))),
+          @ApiResponse(
+              responseCode = "400",
+              description = "Bad Request",
+              content =
+              @Content(
+                  mediaType = MediaType.APPLICATION_JSON_VALUE,
+                  schema = @Schema(implementation = ProblemJson.class))),
+          @ApiResponse(
+              responseCode = "401",
+              description = "Unauthorized",
+              content = @Content(schema = @Schema())),
+          @ApiResponse(
+              responseCode = "403",
+              description = "Forbidden",
+              content = @Content(schema = @Schema())),
+          @ApiResponse(
+              responseCode = "429",
+              description = "Too many requests",
+              content = @Content(schema = @Schema())),
+          @ApiResponse(
+              responseCode = "500",
+              description = "Service unavailable",
+              content =
+              @Content(
+                  mediaType = MediaType.APPLICATION_JSON_VALUE,
+                  schema = @Schema(implementation = ProblemJson.class)))
+      })
+  @GetMapping("/info")
+  public ResponseEntity<AppInfo> healthCheck() {
+
+    AppInfo info =
+        AppInfo.builder()
+            .name(name)
+            .version(version)
+            .environment(environment)
+            .build();
+    return ResponseEntity.status(HttpStatus.OK).body(info);
   }
 }
