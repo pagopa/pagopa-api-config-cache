463b8f0fdfe67f6311552cbb9f523651ad3c424e
Cesare Caccuri <cesare.caccuri@gmail.com>
2023-05-08 11:20:45 +0200

    openapi

--- openapi/generate_openapi.sh
+++ openapi/generate_openapi.sh
@@ -12,5 +12,6 @@ fi
 # save openapi
 curl http://localhost:8080/v3/api-docs/nodev1 > ./openapi_nodev1.json
 curl http://localhost:8080/v3/api-docs/verifier > ./openapi_verifier.json
+curl http://localhost:8080/v3/api-docs/fdrv1 > ./openapi_fdrv1.json
 
 # UI mode http://localhost:8080/swagger-ui/index.html
