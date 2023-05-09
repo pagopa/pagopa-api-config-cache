23939df868a39058cc80473544659f105eaf67e6
Cesare Caccuri <cesare.caccuri@gmail.com>
2023-05-09 09:13:39 +0200

    fix tests

--- openapi/generate_openapi.sh
+++ openapi/generate_openapi.sh
@@ -11,7 +11,7 @@ fi
 
 # save openapi
 curl http://localhost:8080/v3/api-docs/nodev1 > ./openapi_nodev1.json
-curl http://localhost:8080/v3/api-docs/verifier > ./openapi_verifier.json
+curl http://localhost:8080/v3/api-docs/verifier > ./openapi_verifierv1.json
 curl http://localhost:8080/v3/api-docs/fdrv1 > ./openapi_fdrv1.json
 
 # UI mode http://localhost:8080/swagger-ui/index.html
