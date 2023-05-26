9caba5e33caf582a37c714260e1934ca8113d547
Cesare Caccuri <cesare.caccuri@gmail.com>
2023-05-26 18:33:14 +0200

    added oracleprod

--- src/test/java/it/gov/pagopa/apiconfig/cache/StaticTests.java
+++ src/test/java/it/gov/pagopa/apiconfig/cache/StaticTests.java
@@ -2,6 +2,8 @@ package it.gov.pagopa.apiconfig.cache;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import it.gov.pagopa.apiconfig.cache.model.NodeCacheKey;
+import it.gov.pagopa.apiconfig.cache.model.StringToNodeCacheKeyConverter;
 import it.gov.pagopa.apiconfig.cache.model.node.v1.ConfigDataV1;
 import it.gov.pagopa.apiconfig.cache.redis.ObjectRedisSerializer;
 import org.junit.jupiter.api.Test;
@@ -17,6 +19,12 @@ class StaticTests {
     assertThat(serializer.deserialize(serialized).equals(cd));
 
     assertThat(serializer.deserialize(null)==null);
+  }
+
+  @Test
+  void checkKeyConverter(){
+    StringToNodeCacheKeyConverter stringToNodeCacheKeyConverter = new StringToNodeCacheKeyConverter();
+    assertThat(stringToNodeCacheKeyConverter.convert("psps").equals(NodeCacheKey.PSPS));
   }
 
 }
