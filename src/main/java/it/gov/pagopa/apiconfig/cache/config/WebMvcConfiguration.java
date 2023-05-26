dfe6a398e0a33c4dc50a749dc432f2fd15424eba
Cesare Caccuri <cesare.caccuri@gmail.com>
2023-05-26 17:45:54 +0200

    added oracleprod

--- src/main/java/it/gov/pagopa/apiconfig/cache/config/WebMvcConfiguration.java
+++ src/main/java/it/gov/pagopa/apiconfig/cache/config/WebMvcConfiguration.java
@@ -2,9 +2,11 @@ package it.gov.pagopa.apiconfig.cache.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.gov.pagopa.apiconfig.cache.model.AppCorsConfiguration;
+import it.gov.pagopa.apiconfig.cache.model.StringToNodeCacheKeyConverter;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -25,5 +27,10 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         .addMapping("/**")
         .allowedOrigins(appCorsConfiguration.getOrigins())
         .allowedMethods(appCorsConfiguration.getMethods());
+  }
+
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+    registry.addConverter(new StringToNodeCacheKeyConverter());
   }
 }
