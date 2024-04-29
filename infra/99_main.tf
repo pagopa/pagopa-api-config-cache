d0ae34cd1296d09a0de4075936b374dce1b4b3c5
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-04-29 11:22:58 +0200

    upgrade azurerm and module version

--- infra/99_main.tf
+++ infra/99_main.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.30.0"
+      version = "<= 3.95.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.30.0"
+      version = "<= 2.47.0"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "= 1.3.0"
+      version = "<= 1.3.0"
     }
   }
 
