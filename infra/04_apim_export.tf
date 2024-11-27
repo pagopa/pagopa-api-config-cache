850659679acc02d57b8430a9960125b97c9c15be
Francesco Cesareo <cesareo.francesco@gmail.com>
2024-11-27 11:02:39 +0100

    added prf export api

--- infra/04_apim_export.tf
+++ infra/04_apim_export.tf
@@ -1,5 +1,4 @@
 # PostgreSQL
-
 resource "azurerm_api_management_api_version_set" "api_apiconfig_cache_export_node_api_p" {
   name                = "${var.env_short}-${var.domain}-cache-export-${local.postgres}-api"
   resource_group_name = local.apim.rg
@@ -80,11 +79,12 @@ module "apim_api_apiconfig_cache_export_node_api_v1_p" {
 #}
 
 resource "azurerm_api_management_api_version_set" "api_apiconfig_cache_replica_export_node_api_p" {
-  count               = var.env_short == "p" ? 0 : 1
+  count               = var.env_short == "u" ? 1 : 0
+
   name                = "${var.env_short}-${var.domain}-cache-replica-export-${local.postgres}-api"
   resource_group_name = local.apim.rg
   api_management_name = local.apim.name
-  display_name        = "${local.apiconfig_cache_replica_export_locals.display_name} - ${local.postgres}"
+  display_name        = "${local.apiconfig_cache_replica_export_locals.display_name} - ${local.postgres}r"
   versioning_scheme   = "Segment"
 }
 
@@ -99,12 +99,12 @@ resource "azurerm_api_management_api_version_set" "api_apiconfig_cache_replica_e
 
 module "apim_api_apiconfig_cache_replica_export_node_api_v1_p" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v8.5.0"
-  count  = var.env_short == "p" ? 0 : 1
+  count               = var.env_short == "u" ? 1 : 0
 
   name                = "${var.env_short}-${var.domain}-cache-replica-export-${local.postgres}-api"
   api_management_name   = local.apim.name
   resource_group_name   = local.apim.rg
-  product_ids           = [local.apim.product_id, local.technical_support_product_id]
+  product_ids           = [local.apim.product_id, local.technical_support_product_id, local.cache_export_product_id]
   subscription_required = local.apiconfig_cache_replica_export_locals.subscription_required
 
   version_set_id = azurerm_api_management_api_version_set.api_apiconfig_cache_replica_export_node_api_p[0].id
